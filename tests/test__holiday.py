157360a98adff39aa5114415b213833acc49138a
Gunhoon Lee <gunhoon@gmail.com>
2025-05-15 00:18:10 +0900

    Update holiday info for 2025

--- tests/test__holiday.py
+++ tests/test__holiday.py
@@ -23,9 +23,9 @@ def test_holiday_from_file():
     assert info[17] == '2024-12-31'
 
     info = holiday_from_file(2025)
-    assert len(info) == 18
+    assert len(info) == 19
     assert info[0] == '2025-01-01'
-    assert info[17] == '2025-12-31'
+    assert info[18] == '2025-12-31'
 
     info = holiday_from_file(2026)
     assert len(info) == 0
@@ -34,9 +34,9 @@ def test_holiday_from_file():
 @pytest.mark.skipif(False, reason='requires http request')
 def test_holiday_from_krx():
     info = holiday_from_krx(2025)
-    assert len(info) == 18
+    assert len(info) == 19
     assert info[0] == '2025-01-01'
-    assert info[17] == '2025-12-31'
+    assert info[18] == '2025-12-31'
 
 
 @pytest.mark.skipif(False, reason='requires http request')
