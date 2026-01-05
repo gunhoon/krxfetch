72a247960fca7ad036b17b9358da9ba840ffb7fb
Gunhoon Lee <gunhoon@gmail.com>
2026-01-06 00:54:02 +0900

    KRX site 변경에 따른 대응

--- src/krxfetch/fetch.py
+++ src/krxfetch/fetch.py
@@ -5,14 +5,14 @@ from . import _chrome
 
 def get_json_data(payload: dict, referer: str | None = None) -> list[dict]:
     if referer is None:
-        referer = 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201'
+        referer = 'https://data.krx.co.kr/contents/MDC/MDI/outerLoader/index.cmd?menuId=MDC0201'
 
     headers = {
         'user-agent': _chrome.user_agent(),
         'referer': referer
     }
 
-    url = 'http://data.krx.co.kr/comm/bldAttendant/getJsonData.cmd'
+    url = 'https://data.krx.co.kr/comm/bldAttendant/getJsonData.cmd'
 
     r = requests.post(url=url, data=payload, headers=headers)
     json = r.json()
@@ -26,13 +26,17 @@ def get_json_data(payload: dict, referer: str | None = None) -> list[dict]:
     return json[k]
 
 
-def download_csv(payload: dict) -> str:
+def download_csv(payload: dict, referer: str | None = None) -> str:
+    if referer is None:
+        referer = 'https://data.krx.co.kr/contents/MDC/MDI/outerLoader/index.cmd?menuId=MDC0201'
+
     headers = {
-        'user-agent': _chrome.user_agent()
+        'user-agent': _chrome.user_agent(),
+        'referer': referer
     }
 
     # 1. Generate OTP
-    otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
+    otp_url = 'https://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
 
     r = requests.post(url=otp_url, data=payload, headers=headers)
     otp = {
@@ -40,7 +44,7 @@ def download_csv(payload: dict) -> str:
     }
 
     # 2. Download CSV
-    url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
+    url = 'https://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
 
     r = requests.post(url=url, data=otp, headers=headers)
     csv = r.content.decode(encoding='euc_kr')
