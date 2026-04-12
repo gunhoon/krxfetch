e8ca31ab25f3ad4a7b08c29380db01377e5464fc
Gunhoon Lee <gunhoon@gmail.com>
2026-04-13 00:22:05 +0900

    Session Object 도입

--- src/krxfetch/fetch.py
+++ src/krxfetch/fetch.py
@@ -5,6 +5,7 @@ from . import _chrome
 
 class Fetch:
     def __init__(self):
+        self.session = requests.Session()
         self.referer = 'https://data.krx.co.kr/contents/MDC/MDI/outerLoader/index.cmd?menuId=MDC0201'
 
     def get_json_data(self, payload: dict) -> list[dict]:
@@ -15,7 +16,7 @@ class Fetch:
 
         url = 'https://data.krx.co.kr/comm/bldAttendant/getJsonData.cmd'
 
-        r = requests.post(url=url, data=payload, headers=headers)
+        r = self.session.post(url=url, headers=headers, data=payload)
         json = r.json()
 
         keys = list(json)
@@ -35,7 +36,7 @@ class Fetch:
         # 1. Generate OTP
         otp_url = 'https://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
 
-        r = requests.post(url=otp_url, data=payload, headers=headers)
+        r = self.session.post(url=otp_url, headers=headers, data=payload)
         otp = {
             'code': r.text
         }
@@ -43,7 +44,7 @@ class Fetch:
         # 2. Download CSV
         url = 'https://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
 
-        r = requests.post(url=url, data=otp, headers=headers)
+        r = self.session.post(url=url, headers=headers, data=otp)
         csv = r.content.decode(encoding='euc_kr')
 
         return csv
